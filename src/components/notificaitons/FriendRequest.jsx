0c5623646097a15bc9347ca8a63094316f883128
guillermoFragachan <guillefragachan@gmail.com>
2021-11-17 14:10:50 +0100

    requests

--- src/components/notificaitons/FriendRequest.jsx
+++ src/components/notificaitons/FriendRequest.jsx
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from "react";
 import Dropdown from 'react-bootstrap/Dropdown'
-
-
+import Button from 'react-bootstrap/Button'
 import { IconButton } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import { FastRewind } from "@material-ui/icons";
 
 function FriendRequest({ title, Icon, to }) {
-
- const fetchFriendRequests = async () => {
+    const [friendRequests, setFriendRequests] = useState([]);
+    const fetchFriendRequests = async () => {
     const response = await fetch(`https://linkedin-backend-strive.herokuapp.com/request`);
-    const data = await response.json();
+     const data = await response.json();
+     setFriendRequests(data);
     console.log(data)
   }
  
+
+
 
  useEffect(() => {
     fetchFriendRequests()
@@ -23,17 +26,30 @@ function FriendRequest({ title, Icon, to }) {
   return (
     <>
       
-      <Dropdown>
-        <Dropdown.Toggle  id="dropdown-basic">
-        <IconButton className="header_icons"  to={to}>
+      <Dropdown >
+        <Dropdown.Toggle id="dropdown-basic" >
+        <IconButton className="header_icons" onClick={()=>{
+            console.log(friendRequests)
+        }}  to={to}>
                 <Icon className="header_icons" src={Icon} />
             </IconButton>
         </Dropdown.Toggle>
 
   <Dropdown.Menu>
-    {/* <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item> */}
+   
+    { friendRequests.length >0  &&
+   
+
+        friendRequests.map(request => (
+        <Dropdown.Item href="#/action-3">Request from: {request.userReceived}
+        <Button variant="primary" onClick={()=>{}}>Accept</Button>
+        <Button variant="danger" onClick={()=>{}}>Reject</Button>
+        
+        </Dropdown.Item>
+            
+        )
+        )
+    }
 
 
   </Dropdown.Menu>
