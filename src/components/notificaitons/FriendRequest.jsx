77501b5c337f60f24dc871552f73218503a24cda
guillermoFragachan <guillefragachan@gmail.com>
2021-11-17 15:41:24 +0100

    friendReuqest styles and accesible only to logged user

--- src/components/notificaitons/FriendRequest.jsx
+++ src/components/notificaitons/FriendRequest.jsx
@@ -4,17 +4,55 @@ import Button from 'react-bootstrap/Button'
 import { IconButton } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import { FastRewind } from "@material-ui/icons";
+import './css/FriendRequest.css'
 
 function FriendRequest({ title, Icon, to }) {
     const [friendRequests, setFriendRequests] = useState([]);
-    const fetchFriendRequests = async () => {
-    const response = await fetch(`https://linkedin-backend-strive.herokuapp.com/request`);
-     const data = await response.json();
-     setFriendRequests(data);
-    console.log(data)
-  }
- 
 
+
+
+    const fetchFriendRequests = async () => {
+      const response = await fetch(`https://linkedin-backend-strive.herokuapp.com/profile/6194d84aad6c687f678236e0`);
+       const data = await response.json();
+       const array = data.friendRequests;
+       setFriendRequests(array);
+  
+  
+  
+      console.log(array, "array");
+    }
+
+  //   const fetchFriendRequests = async () => {
+  //   const response = await fetch(`https://linkedin-backend-strive.herokuapp.com/request`);
+  //    const data = await response.json();
+  //    setFriendRequests(data);
+
+
+
+  //   console.log(data)
+  // }
+ 
+const acceptFriendRequests = async (id) => {
+    console.log(id)
+    const response = await fetch(`https://linkedin-backend-strive.herokuapp.com/request/${id}`, {
+        method: "PUT",
+        body: JSON.stringify({ status: 'accepted' }),
+
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    if(response.ok){
+        console.log("accepted")
+        const data = await response.json();
+
+     fetchFriendRequests()
+
+    }
+
+     
+    
+  }
 
 
  useEffect(() => {
@@ -27,7 +65,7 @@ function FriendRequest({ title, Icon, to }) {
     <>
       
       <Dropdown >
-        <Dropdown.Toggle id="dropdown-basic" >
+        <Dropdown.Toggle className='btn-primary-remove' id="dropdown-basic" >
         <IconButton className="header_icons" onClick={()=>{
             console.log(friendRequests)
         }}  to={to}>
@@ -37,12 +75,14 @@ function FriendRequest({ title, Icon, to }) {
 
   <Dropdown.Menu>
    
-    { friendRequests.length >0  &&
+    { 
    
 
         friendRequests.map(request => (
-        <Dropdown.Item href="#/action-3">Request from: {request.userReceived}
-        <Button variant="primary" onClick={()=>{}}>Accept</Button>
+        <Dropdown.Item href="#/action-3">Request from: {request.userSent.name}
+        <Button variant="primary" onClick={()=>{
+            acceptFriendRequests(request._id)
+        }}>Accept</Button>
         <Button variant="danger" onClick={()=>{}}>Reject</Button>
         
         </Dropdown.Item>
