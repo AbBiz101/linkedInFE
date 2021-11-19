03115ebd728e8226b86943dddfb30563a2abf8e3
Abnet Nega <86370531+AbBiz101@users.noreply.github.com>
2021-11-19 13:25:08 +0100

    Revert "all done"
    This reverts commit c8d8b0944e13e78384c721c47fa91a3c0d30a785.

--- src/components/home_page/PostModel.jsx
+++ src/components/home_page/PostModel.jsx
@@ -22,13 +22,10 @@ function ProfileModal({ show, setShow, authorized, getdata }) {
 				'https://linkedin-backend-strive.herokuapp.com/posts',
 				{
 					method: 'POST',
-					body: JSON.stringify({
-						text: values,
-						user: '6194d84aad6c687f678236e0',
-					}),
+					body: JSON.stringify({ text: values }),
 					headers: {
 						'Content-Type': 'application/json',
-						// Authorization: process.env.REACT_APP_API_KEY,
+						Authorization: process.env.REACT_APP_API_KEY,
 					},
 				},
 			);
